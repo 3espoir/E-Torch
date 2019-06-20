bb8dffcc0279a5d9a6d87b75842071abefe44027
Saly <saly@juke4me.com>
2019-06-20 11:27:20 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from '@emotion/styled';
 
 function App() {
+  
+  const Header = styled.header`
+  overflow: visible;
+  width: 100%;
+  position: absolute;
+  z-index: 1;
+  `
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <Header>
+        <png src={logo} className="App-logo" alt="logo" />
+        E-Torch
+      </Header>
     </div>
   );
 }
